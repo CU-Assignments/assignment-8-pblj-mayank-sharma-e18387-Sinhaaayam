f31bffb9d07e87365de0f9d3be64d8fba37830bd
Sinhaaayam <135627202+Sinhaaayam@users.noreply.github.com>
2025-04-23 12:30:46 +0530

    Create Employee.java

---
+++ Medium/Employee.java
@@ -0,0 +1,23 @@
+package com.example.webapp;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee() {}
+
+    public Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    // Getters and Setters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public double getSalary() { return salary; }
+}
